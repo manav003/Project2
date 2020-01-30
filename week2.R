f0f2b1e9fd3b7c0e67d9fb76835afba5a33aa6a0
Mathi Manavalan <manav003@ad.umn.edu>
2020-01-29 19:19:57 -0600

    Final commit, HW complete

--- week2.R
+++ week2.R
@@ -1,3 +1,12 @@
 # Import and Labeling
 rt_df <- read.csv(file = "week2.csv", header = TRUE)
 levels(rt_df$condition) <- c("Control", "Experimental")
+levels(rt_df$gender) <- c("Female", "Male", "Transgender")
+
+#Analysis
+mean(rt_df$rt)
+rt_f_df <- data.frame(subset(rt_df, gender == "Female"))
+summary(rt_f_df)
+hist(rt_f_df$rt)
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]]["rt"]
